2dcad0a2cc97b8d2a999dc0abfb98b67bbbc2402
GRIEGMAK <g.m.ak@yandex.ru>
2021-07-07 13:11:55 +0600

    Блок about доделан

--- src/components/EmotionsAbout.jsx
+++ src/components/EmotionsAbout.jsx
@@ -14,13 +14,18 @@ const EmotionsAbout = () => {
           <div className="image"><img src={img1}/></div>
           <div className="image"><img src={img1}/></div>
           <div className="frame">
-            <div className="frame_group"></div>
-            <div className="frame_text"></div>
+            <div className="frame_icon"></div>
+            <div className="frame_text">Все фото: 99</div>
           </div>
         </div>
       </div>
       <div>
-        text
+        <div className="emotionsAboutTitle">
+          Обзор тура:
+        </div>
+        <div className="emotionsAboutGrid">
+          Среди сыроваров есть тенденция, что они не хотят делиться своими секретами и рецептами. Говорят, что это очень сложное дело...что нужно ехать в Европу или идти в подмастерье к настоящему сыровару на несколько лет, да и то не факт, что тебе раскроют все секреты. Но это не так!
+        </div>
       </div>
     </div>
   );
